f78858947efd64623b9e04a1c448c4a21b22d2fe
khybertailor <khaibertailor@gmail.com>
2024-09-25 22:49:47 -0400

    uploaded website files
    uploaded

---
+++ script.js
@@ -0,0 +1,3 @@
+// For now, there's no specific functionality needed for JavaScript.
+// You can add dynamic features later if required.
+console.log("Khyber Tailoring website loaded successfully.");
